5681f8dc095201ac134a5aa2979ea8bc4ac4002b
Damego <danyabatueff@gmail.com>
2024-02-03 21:10:08 +0500

    ref(models): optional posters

--- lib/src/models/title/posters.dart
+++ lib/src/models/title/posters.dart
@@ -5,9 +5,9 @@ part 'posters.g.dart';
 
 @JsonSerializable()
 class Posters {
-  final Poster small;
-  final Poster medium;
-  final Poster original;
+  final Poster? small;
+  final Poster? medium;
+  final Poster? original;
 
   Posters(this.small, this.medium, this.original);
 
--- lib/src/models/title/posters.g.dart
+++ lib/src/models/title/posters.g.dart
@@ -7,9 +7,15 @@ part of 'posters.dart';
 // **************************************************************************
 
 Posters _$PostersFromJson(Map<String, dynamic> json) => Posters(
-      Poster.fromJson(json['small'] as Map<String, dynamic>),
-      Poster.fromJson(json['medium'] as Map<String, dynamic>),
-      Poster.fromJson(json['original'] as Map<String, dynamic>),
+      json['small'] == null
+          ? null
+          : Poster.fromJson(json['small'] as Map<String, dynamic>),
+      json['medium'] == null
+          ? null
+          : Poster.fromJson(json['medium'] as Map<String, dynamic>),
+      json['original'] == null
+          ? null
+          : Poster.fromJson(json['original'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$PostersToJson(Posters instance) => <String, dynamic>{
